da1184c8de41c2bb8e0f93251453a7082936e37d
John Fullerton <JohnFullerton@Johns-MacBook-Air.local>
2017-08-16 19:12:08 -0700

    adding first draft Article model & server.js

---
+++ models/articles.js
@@ -0,0 +1,59 @@
+// REQUIRED PACKAGES
+
+	// Mongoose NPM
+	const mongoose = require("mongoose");
+	const uniqueValidator = require("mongoose-unique-validator");
+
+	// Create Schema
+	var Schema = mongoose.Schema
+
+	// Create Article Collection Schema
+	var ArticleSchema = new Schema({
+
+		title: {
+			type: String,
+			required: true,
+			unique: true
+		}
+
+		date: {
+			type: String,
+			required: true,
+			unique: true
+
+		}
+		url: {
+			type: String,
+			required: true,
+			unique: true
+		}
+
+		author: {
+			type: String,
+			unique: true
+		}
+		section: {
+			type: String,
+			unique: true
+		}
+		saved: {
+			type: Boolean,
+			default: false
+		}
+
+	});// close ArchicleSchema var
+
+// ACTIVATE MONGOOSE UNIQUE VALIDATOR
+ArchicleSchema.plugin(uniqueValidator);
+
+ArchicleSchema.methods.saveArticle = function() {
+	this.saved = true;
+	return this.saved;
+};// close saveArticle validate funct
+
+
+// CREATE Article Model using Schema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// EXPORT MODEL
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,59 @@
+
+// REQUIRED PACKAGES
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var favicon = require("serv-favicon");
+var path = require("path");
+
+// ACTIVATE EXPRESS & DEFINE PORT
+	var app = express();
+	var PORT = process.env.PORT || 8080;
+
+// Designate Public & Static Assets
+	app.use(express.static(process.cwd() + "/public"));
+
+// Activate Logger/Morgan
+	app.use(logger('dev'));
+
+// Activate Body-Parser
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+
+	app.use(bodyParser.json());
+
+
+// Have Mongoose use built-in JS ES6 Promises
+mongoose.Promise = Promise;
+
+
+// CONNECT TO MongoDB w/ Mongoose
+mongoose.connect("mongodb://localhost/nytreact");
+var db = mongoose.connection;
+
+	// SHOW DB CONNECTION ERRORS
+	db.on("error", function(error) {
+  		console.log("Mongoose Error: ", error);
+	});
+
+	// DB CONNECTION SUCCESS
+	db.once("open", function() {
+  		console.log("Mongoose connection successful.");
+	});
+
+
+// ROUTES
+var htmlRoutes = require("./routes/html-routes.js");
+var apiRoutes = require("./routes/api-routes.js");
+
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+
+
+// ACTIVATE SERVER
+var port = 3000
+app.listen(port, function() {
+	console.log("SERVER LISTENING on: " + port);
+});
